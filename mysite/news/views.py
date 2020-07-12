b04a9fcf4ada072d6c6a4b0d1ee36b594c4ab149
RasimAbasov <abasovrasim.k@gmail.com>
2020-07-12 13:37:38 +0300

    вывод новостей с использованием шаблона

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -5,14 +5,14 @@ from .models import News
 # Create your views here.
 
 
+# def index(request):
+#     # print(request)
+#     news = News.objects.all()
+#     res = '<h1>Список новостей</h1>'
+#     for item in news:
+#         res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+#     return HttpResponse(res)
+
 def index(request):
-    # print(request)
     news = News.objects.all()
-    res = '<h1>Список новостей</h1>'
-    for item in news:
-        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
-    return HttpResponse(res)
-
-
-def test(request):
-    return HttpResponse("<h1>Тестовая страница<h1>")
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
