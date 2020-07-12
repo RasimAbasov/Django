f5044f9ab0c349442d00f1253eb52ae9e3b21b24
RasimAbasov <abasovrasim.k@gmail.com>
2020-07-12 13:52:17 +0300

    вывод новостей с использованием шаблона (директивы, теги, фильтры)

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -14,5 +14,8 @@ from .models import News
 #     return HttpResponse(res)
 
 def index(request):
-    news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+    news = News.objects.order_by('-created_at')
+    context = {'news': news,
+               'title': 'Список новостей'
+               }
+    return render(request, template_name='news/index.html', context=context)
